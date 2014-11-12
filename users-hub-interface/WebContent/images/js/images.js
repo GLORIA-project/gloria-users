0c21a3c852356a50a2ee7c8e940fae1385211fbe
esgg <egonzalez@ciclope.info>
2014-11-12 14:48:03 +0100

    Control for SADIRA

--- users-hub-interface/WebContent/images/js/images.js
+++ users-hub-interface/WebContent/images/js/images.js
@@ -113,3 +113,129 @@ function CarouselCtrl($scope, $gloriaAPI, $timeout, $gloriaLocale) {
 		$scope.loading = false;
 	};
 }
+
+function AlbumFormCtrl($gloriaAPI, $scope, $timeout) {
+	
+	$scope.telescopes = [];
+	
+	
+	$scope.experiments = [
+	   'Solar Experiment',
+	   'Night Experiment',
+	   'Scheduler'
+	];
+	
+	$scope.experiments_selection = [];
+	
+	$scope.toogleExperimentSelection = function(exp) {
+		
+		var experimentIndex = $scope.experiments_selection.indexOf(exp);
+		
+		if (experimentIndex > -1){
+			$scope.experiments_selection.splice(experimentIndex,1);
+		}  else {
+			$scope.experiments_selection.push(exp);
+		}
+	};
+	
+	$scope.telescopes_selection = [];
+	
+	$scope.toogleTelescopeSelection = function(tel) {
+		
+		var telescopeIndex = $scope.telescopes_selection.indexOf(tel);
+		
+		if (telescopeIndex > -1){
+			$scope.telescopes_selection.splice(telescopeIndex, 1);
+		} else {
+			$scope.telescopes_selection.push(tel);
+		}
+		
+	};
+	
+	$scope.today = function() {
+		$scope.dt_begin = new Date();
+		$scope.dt_end = new Date();
+	};
+	$scope.today();
+	
+	$scope.showWeeks = true;
+	$scope.toggleWeeks = function() {
+		$scope.showWeeks = !$scope.showWeeks;
+	};
+
+	$scope.clear = function() {
+		$scope.dt_begin = null;
+		$scope.dt_end = null;
+	};
+	
+	$scope.current = new Date();
+	
+	$scope.disabled = function(date, mode) {
+		return false;
+	};
+
+	$scope.toggleMin = function() {
+		$scope.minDate = ($scope.minDate) ? null : new Date();
+	};
+	$scope.toggleMin();
+
+	$scope.open = function($event,opened) {
+		 $event.preventDefault();
+		 $event.stopPropagation();
+
+		 $scope[opened] = true;
+	};
+
+	$scope.dateOptions = {
+		'year-format' : "'yy'",
+		'starting-day' : 1
+	};
+	
+	
+	$scope.search = function(){
+		
+		console.log("***************** Experiments ****************");
+		console.log($scope.experiments_selection);
+		
+		console.log("***************** Target *********************");
+		if ($scope.target_type=="name"){
+			console.log("Name Selected");
+			console.log("Name:"+$scope.target_name);
+		} else if ($scope.target_type=="coordinates"){
+			console.log("Coordinates Selected");
+			console.log("RA:"+$scope.ra_coordinate);
+			console.log("DEC:"+$scope.dec_coordinate);
+			console.log("RADIUS:"+$scope.radius);
+		} else {
+			console.log("No target selected");
+		}
+		
+		console.log("***************** Telescopes *********************");
+		console.log($scope.telescopes_selection);
+	};
+	
+	$scope.telescopeTimerFunction = function(){
+		$gloriaAPI.getTelescopes(function (success){
+			//$scope.telescopes = success;
+			$scope.loading_telescopes = false;
+			var item;
+			for (var i=0;i<success.length;i++){
+				if (i<success.length/2){
+					item = {name:success[i],col:1};	
+				} else {
+					item = {name:success[i],col:2};
+				}
+				
+				$scope.telescopes.push(item);
+				
+			}
+		}, function (error){
+			$scope.telescopeTimer = $timeout($scope.telescopeTimerFunction, 1000);
+		});
+		
+	};
+	
+	$timeout($scope.telescopeTimerFunction, 1000);
+	
+
+}
